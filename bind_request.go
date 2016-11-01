b294739e13e7209530d3c4b96e84f4c81aed6a16
Kent Rancourt <kent@deis.com>
2016-11-01 17:05:53 -0400

    chore(BindRequest): Remove dead code

--- bind_request.go
+++ bind_request.go
@@ -1,12 +1,5 @@
 package framework
 
-const (
-	// TargetNamespaceKey is the required key for the target namespace value
-	TargetNamespaceKey = "target_namespace"
-	// TargetNameKey is the required key for the target name value
-	TargetNameKey = "target_name"
-)
-
 // BindRequest represents a request to bind to a service.
 type BindRequest struct {
 	InstanceID string
@@ -14,26 +7,4 @@ type BindRequest struct {
 	PlanID     string
 	BindingID  string
 	Parameters map[string]interface{}
-}
-
-// TargetNamespace returns the target namespace in b.Parameters, or an error if it's missing
-func (b BindRequest) TargetNamespace() (string, error) {
-	return b.paramString(TargetNamespaceKey)
-}
-
-// TargetName returns the target name in b.Parameters, or an error if it's missing
-func (b BindRequest) TargetName() (string, error) {
-	return b.paramString(TargetNameKey)
-}
-
-func (b BindRequest) paramString(key string) (string, error) {
-	i, ok := b.Parameters[key]
-	if !ok {
-		return "", errMissing
-	}
-	s, ok := i.(string)
-	if !ok {
-		return "", errNotAString{value: i}
-	}
-	return s, nil
 }
