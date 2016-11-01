469c0e7f310657b3628d6e7673a1c1e188a47b30
Kent Rancourt <kent@deis.com>
2016-11-01 17:52:19 -0400

    chore(common/logger): Cleaning up logger

--- k8s/common.go
+++ k8s/common.go
@@ -2,18 +2,12 @@ package k8s
 
 import (
 	"fmt"
-
-	"github.com/juju/loggo"
 )
 
 const (
 	// TODO: consider making this configurable
 	resourceAPIVersionBase = "steward.deis.io"
 	apiVersionV1           = "v1"
-)
-
-var (
-	logger = loggo.GetLogger("k8s")
 )
 
 func resourceAPIVersion(v string) string {
---
+++ k8s/logger.go
@@ -0,0 +1,9 @@
+package k8s
+
+import (
+	"github.com/juju/loggo"
+)
+
+var (
+	logger = loggo.GetLogger("k8s")
+)
