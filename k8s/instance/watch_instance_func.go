fdc414a93cd54594ad1afbba2a64b2fe9c6c900a
Aaron Schlesinger <aaron@deis.com>
2016-11-17 14:10:08 -0800

    fix(k8s/instance,k8s/loops.go): use the dynamic client for instance operations

--- k8s/instance/watch_instance_func.go
+++ k8s/instance/watch_instance_func.go
@@ -1,25 +1,27 @@
 package instance
 
 import (
+	"strings"
+
 	"github.com/deis/steward-framework/k8s/data"
-	"github.com/deis/steward-framework/k8s/restutil"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/pkg/api/unversioned"
 	"k8s.io/client-go/pkg/watch"
-	"k8s.io/client-go/rest"
 )
 
 // WatchInstanceFunc is the function that returns a watch interface for instance resources
 type WatchInstanceFunc func(namespace string) (watch.Interface, error)
 
+var instanceAPIResource = unversioned.APIResource{
+	Name:       strings.ToLower(data.InstanceKindPlural),
+	Namespaced: true,
+	Kind:       data.InstanceKind,
+}
+
 // NewK8sWatchInstanceFunc returns a WatchInstanceFunc backed by a Kubernetes client
-func NewK8sWatchInstanceFunc(restIface rest.Interface) WatchInstanceFunc {
+func NewK8sWatchInstanceFunc(cl *dynamic.Client) WatchInstanceFunc {
 	return func(namespace string) (watch.Interface, error) {
-		url := restutil.AbsPath(
-			restutil.APIVersionBase,
-			restutil.APIVersion,
-			true,
-			namespace,
-			data.InstanceKindPlural,
-		)
-		return restIface.Get().AbsPath(url...).Watch()
+		resCl := cl.Resource(&instanceAPIResource, namespace)
+		return resCl.Watch(&data.Instance{})
 	}
 }
