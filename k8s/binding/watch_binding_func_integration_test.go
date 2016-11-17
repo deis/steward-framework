0092c6af8be426d82aa348884b2e5e18971257a0
Kent Rancourt <kent@deis.com>
2016-11-17 16:49:27 -0500

    fix(binding): Fix various issues with integration test

--- k8s/binding/watch_binding_func_integration_test.go
+++ k8s/binding/watch_binding_func_integration_test.go
@@ -7,10 +7,11 @@ import (
 	"time"
 
 	"github.com/arschles/assert"
-	"github.com/deis/steward-framework/k8s"
+	"github.com/deis/steward-framework/k8s/clients"
 	"github.com/deis/steward-framework/k8s/data"
 	testk8s "github.com/deis/steward-framework/testing/k8s"
-	"k8s.io/client-go/watch"
+	"k8s.io/client-go/pkg/runtime"
+	"k8s.io/client-go/pkg/watch"
 )
 
 func TestNewK8sWatchBindingFunc(t *testing.T) {
@@ -20,15 +21,15 @@ func TestNewK8sWatchBindingFunc(t *testing.T) {
 		timeout     = 500 * time.Millisecond
 	)
 	assert.NoErr(t, testk8s.EnsureNamespace(ns))
-	restCfg, err := testk8s.NewRESTConfig()
+	restCfg, err := testk8s.GetRESTConfig()
 	assert.NoErr(t, err)
-	dynCl, err := k8s.NewDynamicClient(*restCfg)
+	dynCl, err := clients.NewDynamic(*restCfg)
 	assert.NoErr(t, err)
 	fn := NewK8sWatchBindingFunc(dynCl)
 	watcher, err := fn(ns)
 	assert.NoErr(t, err)
 	defer watcher.Stop()
-	ch := watcher.ResultCh()
+	ch := watcher.ResultChan()
 	unstructuredBinding := runtime.Unstructured{
 		Object: map[string]interface{}{
 			"Kind":       data.BindingKind,
@@ -46,10 +47,10 @@ func TestNewK8sWatchBindingFunc(t *testing.T) {
 	assert.NoErr(t, createErr)
 	select {
 	case evt := <-ch:
-		assert.Equal(t, evt.Type, watch.Added)
+		assert.Equal(t, evt.Type, watch.Added, "type")
 		binding, ok := evt.Object.(*data.Binding)
 		assert.True(t, ok, "returned event object was not a binding")
-		asssert.Equal(t, binding.Kind, data.BindingKind, "kind")
+		assert.Equal(t, binding.Kind, data.BindingKind, "kind")
 		assert.Equal(t, binding.APIVersion, "steward.deis.io/v1", "api version")
 		assert.Equal(t, binding.Name, bindingName, "name")
 		assert.Equal(t, binding.Namespace, ns, "namespace")
