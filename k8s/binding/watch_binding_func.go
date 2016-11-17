77e7f3d67efdfe4337053a28bb11681bd40f60f4
Aaron Schlesinger <aaron@deis.com>
2016-11-16 16:58:23 -0800

    fix(k8s/binding): add integration test for binding watch func

--- k8s/binding/watch_binding_func.go
+++ k8s/binding/watch_binding_func.go
@@ -10,14 +10,16 @@ import (
 // WatchBindingFunc is the function that returns a watch interface for binding resources
 type WatchBindingFunc func(namespace string) (watch.Interface, error)
 
+var bindingAPIResource = unversioned.APIResource{
+	Name:       "",
+	Namespaced: true,
+	Kind:       data.BindingKindPlural,
+}
+
 // NewK8sWatchBindingFunc returns a WatchBindingFunc backed by a Kubernetes client
 func NewK8sWatchBindingFunc(cl *dynamic.Client) WatchBindingFunc {
 	return func(namespace string) (watch.Interface, error) {
-		resCl := cl.Resource(&unversioned.APIResource{
-			Name:       "",
-			Namespaced: true,
-			Kind:       data.BindingKindPlural,
-		}, namespace)
+		resCl := cl.Resource(&bindingAPIResource, namespace)
 		return resCl.Watch(&data.Binding{})
 	}
 }
--- k8s/binding/watch_binding_func_integration_test.go
+++ k8s/binding/watch_binding_func_integration_test.go
@@ -4,19 +4,57 @@ package binding
 
 import (
 	"testing"
+	"time"
 
 	"github.com/arschles/assert"
 	"github.com/deis/steward-framework/k8s"
+	"github.com/deis/steward-framework/k8s/data"
 	testk8s "github.com/deis/steward-framework/testing/k8s"
+	"k8s.io/client-go/watch"
 )
 
 func TestNewK8sWatchBindingFunc(t *testing.T) {
+	const (
+		ns          = "test"
+		bindingName = "testBinding"
+		timeout     = 500 * time.Millisecond
+	)
+	assert.NoErr(t, testk8s.EnsureNamespace(ns))
 	restCfg, err := testk8s.NewRESTConfig()
 	assert.NoErr(t, err)
 	dynCl, err := k8s.NewDynamicClient(*restCfg)
 	assert.NoErr(t, err)
 	fn := NewK8sWatchBindingFunc(dynCl)
-	_, err := fn("default")
+	watcher, err := fn(ns)
 	assert.NoErr(t, err)
-	// TODO: do the watch
+	defer watcher.Stop()
+	ch := watcher.ResultCh()
+	unstructuredBinding := runtime.Unstructured{
+		Object: map[string]interface{}{
+			"Kind":       data.BindingKind,
+			"APIVersion": "steward.deis.io/v1",
+			"Metadata": map[string]string{
+				"Name":      bindingName,
+				"Namespace": ns,
+			},
+			"Spec":   map[string]string{},
+			"Status": map[string]string{},
+		},
+	}
+	resourceCl := dynCl.Resource(&bindingAPIResource, ns)
+	_, createErr := resourceCl.Create(&unstructuredBinding)
+	assert.NoErr(t, createErr)
+	select {
+	case evt := <-ch:
+		assert.Equal(t, evt.Type, watch.Added)
+		binding, ok := evt.Object.(*data.Binding)
+		assert.True(t, ok, "returned event object was not a binding")
+		asssert.Equal(t, binding.Kind, data.BindingKind, "kind")
+		assert.Equal(t, binding.APIVersion, "steward.deis.io/v1", "api version")
+		assert.Equal(t, binding.Name, bindingName, "name")
+		assert.Equal(t, binding.Namespace, ns, "namespace")
+	case <-time.After(timeout):
+		t.Fatalf("didn't receive an event within %s", timeout)
+	}
+
 }
